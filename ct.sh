3c14b4bf6620bfce5090ed653e5a58cf5bcf662d
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-05-16 14:34:22 -0700

    ci: fix ci

--- ct.sh
+++ ct.sh
@@ -2,28 +2,53 @@
 set -ex
 cd "$(dirname "$0")"
 
-# According to `mbedtls-sys/vendor/README.md`, need to install needed pkgs
-python3 -m pip install -r ./mbedtls-sys/vendor/scripts/basic.requirements.txt
-
-# setup stack size to ensure big tests run correctly
-compiler_stack_size=16
-ulimit -s "$(expr "$compiler_stack_size" \* 1024)"
-export RUST_MIN_STACK="$(expr "$compiler_stack_size" \* 1024 \* 1024)"
-
-cd "./mbedtls"
+# Setup environment variables
 
 if [ -z $TRAVIS_RUST_VERSION ]; then
     echo "Expected TRAVIS_RUST_VERSION to be set in env"
     exit 1
 fi
-
+# Setup stack size to ensure big tests run correctly
+compiler_stack_size=16
+ulimit -s "$(expr "$compiler_stack_size" \* 1024)"
+export RUST_MIN_STACK="$(expr "$compiler_stack_size" \* 1024 \* 1024)"
 export CFLAGS_x86_64_fortanix_unknown_sgx="-isystem/usr/include/x86_64-linux-gnu -mlvi-hardening -mllvm -x86-experimental-lvi-inline-asm-hardening"
 export CC_x86_64_fortanix_unknown_sgx=clang-11
 export CARGO_INCREMENTAL=0
 
-# install `cargo-nextest`
-cargo install cargo-nextest --locked
 
+# Setup dependencies and tools
+
+# According to `mbedtls-sys/vendor/README.md`, need to install needed pkgs
+python3 -m pip install -r ./mbedtls-sys/vendor/scripts/basic.requirements.txt
+# function for downloading pre-built `cargo-nextest` on various platform
+download_cargo_nextest() {
+    local platform="$1"
+    local cargo_nextest_hash="$2"
+    local url="$3"
+    echo "Check if need to download pre-built $platform 'cargo-nextest'"
+    if ! echo "${cargo_nextest_hash} ${CARGO_HOME:-$HOME/.cargo}/bin/cargo-nextest" | sha512sum -c; then
+        curl -LsSf "$url" | tar zxf - -C "${CARGO_HOME:-$HOME/.cargo}/bin"
+        echo "${cargo_nextest_hash} ${CARGO_HOME:-$HOME/.cargo}/bin/cargo-nextest" | sha512sum -c
+    fi
+}
+# download pre-built `cargo-nextest`
+platform=$(dpkg --print-architecture)
+case "$platform" in
+  amd64)
+    download_cargo_nextest "amd64" "d22ce5799f3056807fd0cd8223a290c7153a5f084d5ab931fce755c2cabd33f79c0f75542eb724fe07a7ca083f415ec1f84edc46584b06df43d97a0ff91018da" "https://get.nexte.st/0.9.52/linux"
+    ;;
+  arm64)
+    download_cargo_nextest "arm64" "cff3297c84560de8693e7f887fcf6cf33ab0036e27a9debf2b0a0832094555335f34dc30d0f9d1128ce8472dcb4594a3cf33be2357b19dcc94269b58090cc1a9" "https://get.nexte.st/0.9.52/linux-arm"
+    ;;
+  *)
+    echo "Unknown platform"
+    ;;
+esac
+
+
+# Test logic start from here
+cd "./mbedtls"
 if [ "$TRAVIS_RUST_VERSION" == "stable" ] || [ "$TRAVIS_RUST_VERSION" == "beta" ] || [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
     # Install the rust toolchain
     rustup default $TRAVIS_RUST_VERSION
